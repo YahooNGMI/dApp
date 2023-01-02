38f1a22549b8430744d51700ec5697d470089f3b
JohnWRalph <John.Ralph.Git@gmail.com>
2023-01-01 19:31:17 -0600

    Finished main objectives. Basic styling. Lacking IPFS support

--- bundle.js
+++ bundle.js
@@ -4,12 +4,7 @@
 var _axios = _interopRequireDefault(require("axios"));
 var _ethers = require("ethers");
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-// console.log(axios);
-
 const submitButton = document.getElementById("submit-button");
-// we use `import axios from "axios"` which is another way of saying `const axios = require("axios")`
-// it is jsut better supported in the browser!
-
 submitButton.addEventListener("click", function () {
   const addressinput = document.getElementById("address-input");
   console.log(addressinput.value);
@@ -17,8 +12,36 @@ submitButton.addEventListener("click", function () {
   const apiKey = "9kNXbHSC4XEBluKh72dg644UOMK74Y7C";
   const baseNFTURL = `https://eth-mainnet.g.alchemy.com/nft/v2/${apiKey}/getNFTs`;
   // const tokenURL = `https://eth-mainnet.g.alchemy.com/v2/${apiKey}`;
-
   const ownerAddr = addressinput.value;
+  // Construct the axios request:
+  var config = {
+    method: 'get',
+    url: `${baseNFTURL}?owner=${ownerAddr}`
+  };
+  //   Getting eth balance
+  var data = JSON.stringify({
+    "jsonrpc": "2.0",
+    "id": 1,
+    "method": "eth_getBalance",
+    "params": [ownerAddr, 'latest']
+  });
+  var configEth = {
+    method: 'post',
+    url: `https://eth-mainnet.g.alchemy.com/v2/${apiKey}`,
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    data: data
+  };
+  (0, _axios.default)(configEth).then(function (response) {
+    let balance = response['data']['result'];
+    balance = _ethers.ethers.utils.formatEther(balance);
+    console.log(`Balance of ${address}: ${balance} ETH`);
+    document.getElementById("balance").innerHTML = balance;
+  }).catch(function (error) {
+    console.log(error);
+  });
 
   // Construct the axios request:
   var config = {
@@ -26,53 +49,51 @@ submitButton.addEventListener("click", function () {
     url: `${baseNFTURL}?owner=${ownerAddr}`
   };
   (0, _axios.default)(config).then(function (response) {
-    // const nftName = document.getElementById("nft-name");
-    // const imageName = response.data.ownedNfts[0].metadata.name;
     var myNFTs = response.data.ownedNfts;
     console.log(myNFTs);
-    let myNFTList = [];
-    for (let i = 0; i < myNFTs.length; i++) {
-      console.log(response.data.ownedNfts[i].metadata.name);
-      console.log(response.data.ownedNfts[i].metadata.image);
-      myNFTList[i] = response.data.ownedNfts[i].metadata.name;
+    var cont = document.getElementById("container");
+    for (var i = 0; i < myNFTs.length; ++i) {
+      var h1 = document.createElement("h1");
+      var h2 = document.createElement("h2");
+      var h3 = document.createElement("h3");
+      var div = document.createElement("div");
+      div.setAttribute("class", "square");
+      div.setAttribute("id", "div" + i);
+      const displaytitle = document.createTextNode(myNFTs[i].title);
+      const displayDescription = myNFTs[i].metadata.image;
+      var displayBalance = document.createTextNode(myNFTs[i].balance);
+      console.log(displayDescription);
+      const img = document.createElement("img");
+      // displaytitle.classList.add("h1");
+      h1.classList.add("h1");
+      h2.classList.add("h2");
+      img.classList.add("img");
+      h3.classList.add("h3");
+      // img.style.width = "50%";
+      // img.style.height = "50%";
+      img.src = displayDescription;
+      h1.appendChild(displaytitle);
+      h2.appendChild(img);
+      h3.append("Number Owned: ", displayBalance);
+      div.appendChild(h1);
+      div.appendChild(img);
+      div.appendChild(h3);
+      cont.appendChild(div);
     }
-    console.log(myNFTList);
-    document.getElementById("NFT-List").innerHTML = myNFTList;
-    console.log(response.data.ownedNfts[0].metadata.name);
-    document.getElementById("NFTname").innerHTML = response.data.ownedNfts[0].metadata.name;
-    console.log(response.data.ownedNfts[0].metadata.image);
-    document.getElementById("NFTimage").src = response.data.ownedNfts[0].metadata.image;
-    console.log(response.data.ownedNfts[1].metadata.name);
-    document.getElementById("NFTname2").innerHTML = response.data.ownedNfts[1].metadata.name;
-    console.log(response.data.ownedNfts[1].metadata.image);
-    document.getElementById("NFTimage2").src = response.data.ownedNfts[1].metadata.image;
-
-    //   Getting eth balance
-    var data = JSON.stringify({
-      "jsonrpc": "2.0",
-      "id": 1,
-      "method": "eth_getBalance",
-      "params": [ownerAddr, 'latest']
-    });
-    var configEth = {
-      method: 'post',
-      url: `https://eth-mainnet.g.alchemy.com/v2/${apiKey}`,
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      },
-      data: data
-    };
-    (0, _axios.default)(configEth).then(function (response) {
-      let balance = response['data']['result'];
-      balance = _ethers.ethers.utils.formatEther(balance);
-      console.log(`Balance of ${address}: ${balance} ETH`);
-      document.getElementById("balance").innerHTML = balance;
-    }).catch(function (error) {
-      console.log(error);
-    });
   });
 });
+
+// console.log(myNFTList);
+// document.getElementById("NFT-List").innerHTML = myNFTList;
+// console.log(response.data.ownedNfts[0].metadata.name);
+// document.getElementById("NFTname").innerHTML = response.data.ownedNfts[0].metadata.name;
+// console.log(response.data.ownedNfts[0].metadata.image);
+// document.getElementById("NFTimage").src = response.data.ownedNfts[0].metadata.image;
+
+// console.log(response.data.ownedNfts[1].metadata.name);
+// document.getElementById("NFTname2").innerHTML = response.data.ownedNfts[1].metadata.name;
+// console.log(response.data.ownedNfts[1].metadata.image);
+// document.getElementById("NFTimage2").src = response.data.ownedNfts[1].metadata.image;
 
 },{"axios":120,"ethers":170}],2:[function(require,module,exports){
 "use strict";
